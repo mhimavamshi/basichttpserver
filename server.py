6ddfcfc1c8b577a98e7e2a15eb62572aa0990e87
mhimavamshi <himavamshi1212@gmail.com>
2024-09-21 22:21:26 +0530

    commit

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 from socket import socket, AF_INET, SOCK_STREAM, SOL_SOCKET, SO_REUSEADDR
 from HTTPRequest import *
 from HTTPResponse import *
-
+from threading import Thread
 
 class StaticHTTPServer:
     def __init__(self, log=True, **kwargs):
@@ -15,31 +15,36 @@ class StaticHTTPServer:
     def log(self, message):
         if self.should_log: print(message)
 
+    def handle_client(self, client_sock, address):
+        raw_data = client_sock.recv(1024)
+        # self.log("Raw data: "+str(raw_data)+" from "+str(address))
+        # TODO: listen to the http request header info, don't just respond
+        request = HTTPRequest(raw_data)
+        self.log("\n"+str(address)+": "+request.info()+"\n")
+
+        # TODO: handle paths better, a hack for now 
+        file_path = "."+request.path
+
+        if request.method in {'POST', 'HEAD'}:
+            response = HTTPResponse(status_code=400) 
+
+        if request.method == "GET":
+            if not os.path.isfile(file_path):
+                response = HTTPResponse(status_code=404)
+            else:
+                response = HTTPResponse(status_code=200, file_path=file_path)
+
+        client_sock.send(response.make())        
+        self.log(f"Sent \n------\n{response}\n------\n to client with address {address}")
+
     def serve(self):
         print(f"Server will listen at {self.host}:{self.port}")
         self.tcp_sock.listen(5)
         
         while True:
             client_sock, address = self.tcp_sock.accept()
-            raw_data = client_sock.recv(2048)
-            # self.log("Raw data: "+str(raw_data)+" from "+str(address))
-            # TODO: listen to the http request info, don't just respond
-            request = HTTPRequest(raw_data)
-            self.log(str(address)+": "+request.string())
+            client_thread = Thread(target=self.handle_client, args=(client_sock, address))
+            client_thread.start()
 
-            # TODO: handle paths better, a hack for now 
-            file_path = "."+request.path
-
-            if request.method in {'POST', 'HEAD'}:
-                response = HTTPResponse(status_code=400) 
-
-            if request.method == "GET":
-                if not os.path.isfile(file_path):
-                    response = HTTPResponse(status_code=404)
-                else:
-                    response = HTTPResponse(status_code=200, file_path=file_path)
-
-            self.log(f"Sent \n{response}\n to client with address {address}")
-            client_sock.send(response.make())
 
 

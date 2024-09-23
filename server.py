8f5c62cecdd531aa27e6f38e3d68e2fe51c2b3dd
mhimavamshi <himavamshi1212@gmail.com>
2024-09-23 22:01:56 +0530

    use file cache

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from socket import socket, AF_INET, SOCK_STREAM, SOL_SOCKET, SO_REUSEADDR
+from socket import SHUT_RDWR, socket, AF_INET, SOCK_STREAM, SOL_SOCKET, SO_REUSEADDR
 from HTTPRequest import *
 from HTTPResponse import *
 from threading import Thread
@@ -6,13 +6,16 @@ from threading import Thread
 class StaticHTTPServer:
     def __init__(self, log=True, **kwargs):
         self.host, self.port = kwargs['host'], kwargs['port']
+        self.client_threads = []
         self.tcp_sock = socket(AF_INET, SOCK_STREAM)
         self.tcp_sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
         self.tcp_sock.bind((self.host, self.port))
         self.should_log = log
+        self.file_cache = {}
 
-    def log(self, message):
-        if self.should_log: print(message)
+    def log(self, message, mtype="INFO"):
+        if self.should_log: 
+            print(f"[{mtype}]:" , message)
 
     def handle_client(self, client_sock, address):
         raw_data = client_sock.recv(1024)
@@ -22,7 +25,20 @@ class StaticHTTPServer:
         self.log("\n"+str(address)+": "+request.info()+"\n")
 
         # TODO: handle paths better, a hack for now 
-        file_path = "."+request.path
+        file_path = os.path.normpath("." + request.path)
+
+
+        # TODO: handle this when the files on disk change
+        if file_path in self.file_cache:
+            body = self.file_cache[file_path]
+            self.log(f"Got {file_path} from file cache")
+        else:
+            body = []
+            body.append(HTTPResponse.extension_to_mime[file_path.rsplit('.', 1)[-1]])
+            body.append(str(os.path.getsize(file_path)))
+            with open(file_path, "r") as fl: body.append(fl.read())
+            self.file_cache[file_path] = body             
+            self.log(f"Added {file_path} to file cache")
 
         if request.method in {'POST', 'HEAD'}:
             response = HTTPResponse(status_code=400) 
@@ -33,19 +49,37 @@ class StaticHTTPServer:
             elif not os.path.isfile(file_path):
                 response = HTTPResponse(status_code=404)
             else:
-                response = HTTPResponse(status_code=200, file_path=file_path)
+                response = HTTPResponse(status_code=200, body=body)
 
         client_sock.send(response.make())        
         self.log(f"Sent \n------\n{response}\n------\n to client with address {address}")
 
-    def serve(self):
+
+        try:
+            client_sock.shutdown(SHUT_RDWR)
+            client_sock.close()
+        except Exception as exp:
+            self.log(f"{exp} encountered while shutting down client socket", mtype="ERROR")
+
+    def serve_loop(self):
         print(f"Server will listen at {self.host}:{self.port}")
         self.tcp_sock.listen(5)
         
         while True:
             client_sock, address = self.tcp_sock.accept()
             client_thread = Thread(target=self.handle_client, args=(client_sock, address))
+            self.client_threads.append(client_thread)
             client_thread.start()
 
+    def serve(self):
+        try:
+            self.serve_loop()
+        except KeyboardInterrupt:
+            self.log("\nServer shutting down....")
+        finally:
+            for client_thread in self.client_threads:
+                if client_thread.is_alive(): client_thread.join()
+            self.tcp_sock.close()
+            self.log("Server has shut down")
 
 

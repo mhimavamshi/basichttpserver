6a734bf1914b9c8bdbe58a70daee647acb534556
mhimavamshi <himavamshi1212@gmail.com>
2024-09-23 22:01:35 +0530

    use body instead of filepath

--- HTTPResponse.py
+++ HTTPResponse.py
@@ -27,12 +27,12 @@ class HTTPResponse:
         'zip': 'application/zip',
     }
 
-    def __init__(self, status_code=200, headers={}, file_path=None, redirection_location="/index.html"):
+    def __init__(self, status_code=200, headers={}, body=None, redirection_location="/index.html"):
         self.status_code = status_code
         self.redirection_location = redirection_location
         self.headers = {}
         self.start_line = self.make_response_line()
-        self.body = self.make_body(file_path) 
+        self.body = self.make_body(body) 
         self.headers = self.make_headers(headers)
         self.raw_message = self.make()
 
@@ -45,7 +45,7 @@ class HTTPResponse:
         return f"HTTP/1.0 {self.status_code} {HTTPResponse.status_readable[self.status_code]}"
 
     # TODO: don't read from disk for every response making, use a "cache"
-    def make_body(self, file_path=None):
+    def make_body(self, body=None):
         if self.status_code == 400:
             self.headers['Content-Type'] = "text/plain"
             self.headers['Content-Length'] = "18"
@@ -61,21 +61,22 @@ class HTTPResponse:
             self.headers['Content-Length'] = "0"
             return "\r\n"
 
-        if self.status_code == 200 and file_path == None:
-            raise Exception("No file was provided to serve the GET request.")
+        # if self.status_code == 200 and file_path == None:
+        #     raise Exception("No file was provided to serve the GET request.")
 
-        try:
-            self.headers["Content-Type"] = HTTPResponse.extension_to_mime[file_path.rsplit('.', 1)[-1]]
-        except KeyError:
-            # TODO: is 200 a good idea?
-            print(file_path.rsplit('.', 1)[-1])
-            self.headers["Content-Length"] = "0"
-            return "\r\n"
-        
-        self.headers["Content-Length"] = str(os.path.getsize(file_path))
-        with open(file_path, "r") as fl:
-            body = fl.read()
-        return "\r\n" + str(body)
+        # try:
+        #     self.headers["Content-Type"] = HTTPResponse.extension_to_mime[file_path.rsplit('.', 1)[-1]]
+        # except KeyError:
+        #     # TODO: is 200 a good idea?
+        #     print(file_path.rsplit('.', 1)[-1])
+        #     self.headers["Content-Length"] = "0"
+        #     return "\r\n"
+
+        self.headers["Content-Type"] = body[0]
+        self.headers["Content-Length"] = body[1]
+        # self.headers["Expires"] = 
+        # self.headers["Last-Modified"] =
+        return "\r\n" + str(body[2])
 
 
     def make(self):

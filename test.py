a3e0d1b4e5303276965be8a9d14f0f86d397ea3d
mhimavamshi <himavamshi1212@gmail.com>
2024-09-26 12:47:07 +0530

    moved to src

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
-from server import StaticHTTPServer
+from src.server import StaticHTTPServer
+from src.utils.expiryreader import ExpiryReader
 
-http_server = StaticHTTPServer(host='0.0.0.0', port=5000)
+reader = ExpiryReader("./expires.info")
+
+http_server = StaticHTTPServer(host='0.0.0.0', port=5000, expiryreader=reader)
 # no freedom here.... serves the current directory
 http_server.serve()
